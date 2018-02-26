f25d6f7ad3109cf88d941cf39d552777a7477108
Hung Nguyen Ba <hung.nb@temando.com>
2018-02-26 23:58:15 +0700

    updated status code for user controller and post controller

--- src/modules/posts/post.controller.js
+++ src/modules/posts/post.controller.js
@@ -1,10 +1,20 @@
 import Post from './post.model';
+import HTTPStatus from 'http-status';
 
 export async function createPost(req, res) {
   try {
     const post = await Post.createPost(req.body, req.user._id);
-    return res.status(201).json(post);
+    return res.status(HTTPStatus.CREATED).json(post);
   } catch (error) {
-    return res.status(400).json(error);
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
+  }
+}
+
+export async function getPostById(req, res) {
+  try {
+    const post = await Post.findById(req.param.id);
+    return res.status(HTTPStatus.OK).json(post);
+  } catch (error) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
   }
 }
--- src/modules/users/user.controllers.js
+++ src/modules/users/user.controllers.js
@@ -1,15 +1,16 @@
 import User from './user.model';
+import HTTPStatus from 'http-status';
 
 export async function signup(req, res) {
   try {
     const user = await User.create(req.body);
-    return res.status(201).json(user);
+    return res.status(HTTPStatus.CREATED).json(user.toAuthJSON());
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
   }
 }
 
 export function login(req, res, next) {
-  res.status(200).json(req.user);
+  res.status(HTTPStatus.OK).json(req.user.toAuthJSON());
   return next();
 }
